2907e076403ddff9a0f0dbb2e033a897ff58b0af
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-04-11 15:31:26 +0900

    chore: `react-router` 설치 및 설정

---
+++ src/router/index.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default function Router() {
+  return <RouterProvider router={router} />;
+}
