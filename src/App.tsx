43eeebf2cfa4b43c5eb725697ef4fbba6ae0027d
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-04-12 17:40:05 +0900

    feat: `스크롤 위치 유지`  기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 export default function App() {
   return (
     <RecoilRoot>
+      <ScrollRestoration getKey={(location) => location.pathname} />
       <Outlet />
     </RecoilRoot>
   );
